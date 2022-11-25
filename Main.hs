90a59da89f5dcc966ceb241e87b54d3275ffe525
Alejandro405 <80174674+Alejandro405@users.noreply.github.com>
2022-11-25 14:00:23 +0100

    Recursos de teoría y ejercicios

--- Main.hs
+++ Main.hs
@@ -1,4 +1,18 @@
+module Main where
+    
+
+main :: IO ()
 main = print (fac 20)
+
+
 
 fac 0 = 1
 fac n = n * fac (n-1)
+
+
+clear :: String
+clear = ":! clear"
+
+reciprocal :: (Eq a, Fractional a) => a -> a
+reciprocal x | x == 0 = error "Pobre diabla"
+             | otherwise = 1 / (x + 1)
